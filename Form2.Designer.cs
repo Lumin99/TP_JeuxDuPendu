32089def8592d97eef98cf87282259d3aca17632
Lumin99 <axel.ngot@gmail.com>
2021-02-14 02:06:53 +0100

    Initial commit

---
+++ Form2.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace JeuxDuPendu
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.lCrypedWord = new System.Windows.Forms.Label();
+            this.bReset = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(264, 283);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(255, 38);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "--";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lCrypedWord
+            // 
+            this.lCrypedWord.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lCrypedWord.Location = new System.Drawing.Point(264, 337);
+            this.lCrypedWord.Name = "lCrypedWord";
+            this.lCrypedWord.Size = new System.Drawing.Size(255, 39);
+            this.lCrypedWord.TabIndex = 2;
+            this.lCrypedWord.Text = "--";
+            this.lCrypedWord.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lCrypedWord.Click += new System.EventHandler(this.lCrypedWord_Click);
+            // 
+            // bReset
+            // 
+            this.bReset.Location = new System.Drawing.Point(349, 403);
+            this.bReset.Name = "bReset";
+            this.bReset.Size = new System.Drawing.Size(90, 35);
+            this.bReset.TabIndex = 1;
+            this.bReset.Text = "Nouvelle partie";
+            this.bReset.UseVisualStyleBackColor = true;
+            this.bReset.Click += new System.EventHandler(this.bReset_Click);
+            this.bReset.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.bReset_KeyPress);
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(263, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(256, 256);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(623, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 88);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "J2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(51, 12);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 88);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "J1";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(32, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(178, 145);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "SCORE";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(585, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(178, 145);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "SCORE";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form2
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lCrypedWord);
+            this.Controls.Add(this.bReset);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form2";
+            this.Text = "Form2";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lCrypedWord;
+        private System.Windows.Forms.Button bReset;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ GameForm.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using JeuxDuPendu.MyControls;
+
+
+namespace JeuxDuPendu
+{
+    public partial class GameForm : Form
+    {
+        Random alea = new Random();
+        List<string> ListeMot = new List<string>() /*{ "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi", "Dimanche" }*/;
+        int counter,x;
+        //string filepath = "‪C:\JeuxDuPendu\ListeDeMot.txt";
+        StreamReader reader = new StreamReader(@"..\..\Resources\ListeDeMot.txt");
+        string ligne;
+
+        String mot;
+
+        List<char> Lettre = new List<char>();
+        int nTab;
+        int Verif;
+
+        // Initialisation de l'instance de la classe d'affichage du pendu.
+        HangmanViewer _HangmanViewer = new HangmanViewer();
+
+        /// <summary>
+        /// Constructeur du formulaire de jeux
+        /// </summary>
+        public GameForm()
+        {
+            InitializeComponent();
+            InitializeMyComponent();
+            StartNewGame();
+        }
+
+        /// <summary>
+        /// Initialisations des composant specifique a l'application
+        /// </summary>
+        private void InitializeMyComponent()
+        {
+            // On positionne le controle d'affichage du pendu dans panel1 : 
+            panel1.Controls.Add(_HangmanViewer);
+			
+			// à la position 0,0
+            _HangmanViewer.Location = new Point(0, 0);
+			
+			// et de la même taille que panel1
+            _HangmanViewer.Size = panel1.Size;
+        }
+
+        /// <summary>
+        /// Initialise une nouvelle partie
+        /// </summary>
+        public void StartNewGame()
+        {
+            
+            _HangmanViewer.Reset();
+                        
+            Lettre.Clear();
+            nTab = 0;
+            Verif = 0;
+            int i;
+
+            ligne = reader.ReadLine();
+            for (i = 0; ligne != null; i++)  // Pour chaque ligne
+            {
+                ListeMot.Add(ligne);
+                ligne = reader.ReadLine(); // On passe à la ligne suivante
+            }
+
+            counter = 0;
+            foreach (string m in ListeMot)
+            {
+                counter++;
+            }
+            x = alea.Next(counter);
+            mot = ListeMot[x].ToString().ToUpper();
+            //String mot = "ESIEE";
+
+            // Methode de reinitialisation classe d'affichage du pendu.
+            _HangmanViewer.Init(mot.Length);
+            //Affichage du mot à trouver dans le label.
+            lCrypedWord.Text = "";
+            
+            for (i = 0; i < mot.Length; i++)
+            {
+                lCrypedWord.Text += '_';
+            }
+
+            label1.Text = "";
+        }
+
+
+        /// <summary>
+        /// Methode appelé lors de l'appui d'un touche du clavier, lorsque le focus est sur le bouton "Nouvelle partie"
+        /// </summary>
+        private void bReset_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyPressed(e.KeyChar);
+        }
+
+        /// <summary>
+        /// Methode appelé lors de l'appui d'un touche du clavier, lorsque le focus est sur le forulaire
+        /// </summary>
+        private void GameForm_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            KeyPressed(e.KeyChar);
+        }
+
+        /// <summary>
+        /// Methode appelé lors de l'appui sur le bouton "Nouvelle partie"
+        /// </summary>
+        private void bReset_Click(object sender, EventArgs e)
+        {
+            StartNewGame();
+        }
+
+        private bool LetterNotInTab(char l)
+        {
+            if(nTab != 0)
+            {
+                foreach (char caractere in Lettre)
+                {
+                    if (l == caractere)
+                    {
+                        return false;
+                    }
+                }
+            }
+            
+            Lettre.Add(l);
+            nTab++;
+            return true;
+        }
+
+        private void KeyPressed(char letter)
+        {
+            //HEEEEEERRRRRRRREEEEEEE
+            letter = Char.ToUpper(letter);
+            if (LetterNotInTab(letter)) {
+                if (mot.Contains(letter))
+                {
+                    int i;
+                    for (i = 0; i < mot.Length; i++)
+                    {
+                        if (letter == mot[i])
+                        {
+                            String before = lCrypedWord.Text.Remove(i) + letter;
+                            String after = lCrypedWord.Text.Remove(0, i + 1);
+                            lCrypedWord.Text = before + after;
+                            _HangmanViewer.Incrémenter();
+                        }
+                    }
+                }
+                else
+                {
+                    // On avance le pendu d'une etape
+                    _HangmanViewer.MoveNextStep();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Veuillez taper une lettre non présente dans le tableau");
+            }
+
+            if (Verif+1 == nTab) {
+                label1.Text += Char.ToString(Lettre[nTab - 1]) + ' ';
+                Verif = nTab;
+            }
+                        
+
+            // Si le pendu est complet, le joueur à perdu.
+            if (_HangmanViewer.IsGameOver)
+            {
+                MessageBox.Show("Vous avez perdu !");
+                StartNewGame();
+            }
+            else
+            {   
+                if (_HangmanViewer.IsGameSuccesful)
+                {
+                    MessageBox.Show("Vous avez gagné !");
+                    StartNewGame();
+                }
+            }
+        }
+
+        private void GameForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lCrypedWord_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
